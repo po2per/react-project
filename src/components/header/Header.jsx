a934e4edf0a27819c89c02a82fa0f99778d86ef7
po2per <po2per@mail.ru>
2022-12-26 02:23:21 +0700

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import HeadrSocials from './HeadrSocials'
 import ME from '../../assets/me.png'
+
 
 const Header = () => {
   return (
@@ -13,10 +15,12 @@ const Header = () => {
           Fullstack Developer
         </h5>
         <CTA />
-
+        <HeadrSocials />
         <div className="me">
           <img src={ME} alt="it" />
         </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
--- src/components/header/HeadrSocials.jsx
+++ src/components/header/HeadrSocials.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {FaDribbbleSquare} from 'react-icons/fa'
 
 const HeadrSocials = () => {
   return (
     <div className="header__socials">
-        
+        <a href="#" target="_blank"><BsLinkedin size={'1.5em'}/></a>
+        <a href="#" target="_blank"><BsGithub size={'1.5em'}/></a>
+        <a href="#" target="_blank"><FaDribbbleSquare size={'1.5em'}/></a>
     </div>
   )
 }
